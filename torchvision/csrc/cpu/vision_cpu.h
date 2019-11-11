b66babd78c55e5cf9806aa3940652a2f30fb50cb
Pedro Freire <pfr@fb.com>
2019-11-11 17:18:55 +0000

    Fix pair

--- torchvision/csrc/cpu/vision_cpu.h
+++ torchvision/csrc/cpu/vision_cpu.h
@@ -89,9 +89,9 @@ at::Tensor DCN_forward_cpu(
     const at::Tensor& input,
     const at::Tensor& offset,
     const at::Tensor& weights,
-    const std::pair<int>& stride,
-    const std::pair<int>& pad,
-    const std::pair<int>& dilation,
+    const std::pair<int, int>& stride,
+    const std::pair<int, int>& pad,
+    const std::pair<int, int>& dilation,
     const int groups,
     const int deformable_groups,
     const int im2col_step);
