3ce7130cafb3dcfffa659fcd5a7a49cc80b4f79b
Pedro Freire <pfr@fb.com>
2019-11-11 18:49:05 +0000

    c

--- torchvision/csrc/dcn.h
+++ torchvision/csrc/dcn.h
@@ -87,9 +87,9 @@ Tensor dcn(
     int64_t stride_h, int64_t stride_w,
     int64_t pad_h, int64_t pad_w,
     int64_t dilation_h, int64_t dilation_w,
-    const int64_t groups,
-    const int64_t deformable_groups,
-    const int64_t im2col_step) {
+    int64_t groups,
+    int64_t deformable_groups,
+    int64_t im2col_step) {
   auto result = DCNFunction::apply(input, offset, weights, stride, pad,
                           dilation, groups, deformable_groups, im2col_step);
   return result[0];
