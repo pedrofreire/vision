8955d912e279ba68090b2e43034612ecc37a5bd4
Pedro Freire <pfr@fb.com>
2019-11-15 11:05:46 +0000

    Start adding files

--- torchvision/ops/__init__.py
+++ torchvision/ops/__init__.py
@@ -1,4 +1,5 @@
 from .boxes import nms, box_iou
+from .deform_conv import deform_conv, DeformConv
 from .roi_align import roi_align, RoIAlign
 from .roi_pool import roi_pool, RoIPool
 from .ps_roi_align import ps_roi_align, PSRoIAlign
@@ -12,7 +13,7 @@ _register_custom_op()
 
 
 __all__ = [
-    'nms', 'roi_align', 'RoIAlign', 'roi_pool', 'RoIPool',
-    'ps_roi_align', 'PSRoIAlign', 'ps_roi_pool', 'PSRoIPool',
-    'MultiScaleRoIAlign', 'FeaturePyramidNetwork'
+    'deform_conv', 'DeformConv', 'nms', 'roi_align', 'RoIAlign', 'roi_pool',
+    'RoIPool', 'ps_roi_align', 'PSRoIAlign', 'ps_roi_pool',
+    'PSRoIPool', 'MultiScaleRoIAlign', 'FeaturePyramidNetwork'
 ]
---
+++ torchvision/ops/deform_conv.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn, Tensor
+
+from torch.nn.modules.utils import _pair
+from torch.jit.annotations import List
+
+
+def deform_conv(input, offset, weight,
+        stride=1, pad=0, dilation=1,
+        im2col_step=1):
+    # type: (Tensor) -> Tensor
+    """
+    Performs Deformable Convolution described in Deformable Convolution Networks
+
+    Arguments:
+        input (Tensor[N, C, H, W]): input tensor
+        offset (Tensor[K, 5] or List[Tensor[L, 4]]): the box coordinates in (x1, y1, x2, y2)
+            format where the regions will be taken from. If a single Tensor is passed,
+            then the first column should contain the batch index. If a list of Tensors
+            is passed, then each Tensor will correspond to the boxes for an element i
+            in a batch
+        output_size (int or Tuple[int, int]): the size of the output after the cropping
+            is performed, as (height, width)
+        spatial_scale (float): a scaling factor that maps the input coordinates to
+            the box coordinates. Default: 1.0
+
+    Returns:
+        output (Tensor[K, C, output_size[0], output_size[1]])
+    """
+
+    stride = _pair(stride)
+    pad = _pair(pad)
+    dilation = _pair(dilation)
+
+    stride_h, stride_w = stride
+    pad_h, pad_w = pad
+    dil_h, dil_w = dilation
+    weights_h, weights_w = weight.shape[-2:]
+    _, n_in_channels, in_h, in_w = input.shape
+
+    kernel_h = (weights_h - 1) * dil_h + 1
+    kernel_w = (weights_w - 1) * dil_w + 1
+
+    n_offset_grps = offset.shape[1] // (2 * weights_h * weights_w)
+    n_weight_grps = n_in_channels // weight.shape[1]
+
+    output = torch.ops.torchvision.dcn(
+                input,
+                offset,
+                weight,
+                *stride,
+                *pad,
+                *dilation,
+                n_weight_grps,
+                n_offset_grps,
+                im2col_step)
+    return output
+
+
+class DeformConv(nn.Module):
+    """
+    See dcn
+    """
+    def __init__(self, output_size, spatial_scale):
+        super(DCN, self).__init__()
+        self.output_size = output_size
+        self.spatial_scale = spatial_scale
+
+    def forward(self, input):
+        return dcn(input)
+
+    def __repr__(self):
+        tmpstr = self.__class__.__name__ + '('
+        tmpstr += 'output_size=' + str(self.output_size)
+        tmpstr += ', spatial_scale=' + str(self.spatial_scale)
+        tmpstr += ')'
+        return tmpstr
